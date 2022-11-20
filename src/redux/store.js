7a5ce0f04b3a65538bbebba88b94e902e3c6d5b8
vladdubrovskyi <vladdubrovskyi1993@gmail.com>
2022-11-20 10:54:54 +0200

    add redux toolkit state management

---
+++ src/redux/store.js
@@ -0,0 +1,37 @@
+import { configureStore } from '@reduxjs/toolkit';
+import contactSlice from 'redux/contactSlice';
+import filterReducer from 'redux/filterSlice';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['items'],
+};
+
+const contactsReducer = persistReducer(persistConfig, contactSlice);
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
